7d51439ee20c871043bbe0a2be8518ddc2b796c2
Mario Hdz. Vsqz <brosninja@gmail.com>
2021-04-14 23:54:42 -0700

    clase20210412

---
+++ clase20210412/clase20210412/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  clase20210412
+//
+//  Created by Development on 4/14/21.
+//  Copyright © 2021 Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
